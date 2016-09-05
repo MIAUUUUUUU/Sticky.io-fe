c030f705730b1c22f58a69bd3f543ea8d50507a7
Victor Schulze Buchalla <vs_buchalla@hotmail.com>
2016-09-05 23:41:56 +0000

    Finalizando home

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,10 +1,17 @@
 import Flickity from 'flickity';
+import Fixed from './modules/Fixed.js';
 
-(function(){
-    instantiateFlickities();
-})();
+instantiateFlickities();
+instantiateRanking();
+
 
 function instantiateFlickities () {
-    const mainCarrousel = new Flickity('[data-id="main-carrousel"]', {pageDots: false});    
+    const mainCarrousel = new Flickity('[data-id="main-carrousel"]', {pageDots: false,wrapAround: true});    
 }
+
+function instantiateRanking() {
+    const rank = new Fixed('#ranking');
+    rank.init();
+}
+
 
---
+++ dev/scripts/modules/Fixed.js
@@ -0,0 +1,34 @@
+import $ from 'jquery';
+
+class Fixed {
+    constructor(selector) {
+        this.element = $(selector);
+        this.win = $(window);
+        this.eltop = this.element.offset().top;
+    }
+    
+    init() {
+        this.bindEvents();
+    }
+    
+    bindEvents() {
+        this.win.on('scroll', (e) => {
+           this.handleScroll(e);
+        });
+    }
+    
+    handleScroll(e) {
+        let wintop = this.win.scrollTop();
+        if(wintop >= this.eltop) {
+            let position = wintop - this.eltop;
+            this.setPosition(position);
+        }
+    }
+    
+    setPosition(top) {
+        this.element.css({transform: `translateY(${top}px)`});
+    }
+}
+
+
+export default Fixed;
